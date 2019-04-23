be85f939d7aed47be74615e86e4c3343be916499
DariaTiessova <bermuds@inbox.ru>
2019-04-23 22:27:50 +0600

    onClick

--- Lesson4_animation2/app/src/main/java/com/example/lesson4_animation/MainActivity.java
+++ Lesson4_animation2/app/src/main/java/com/example/lesson4_animation/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.lesson4_animation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.animation.AnticipateOvershootInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    public void onButtonClick(View view) {
+
+
+        ConstraintSet constSet = new ConstraintSet();
+        constSet.clone(this, R.layout.second_activity);
+
+        ConstraintLayout constraint= (ConstraintLayout)findViewById(R.id.container);
+        Transition transition = new ChangeBounds();
+        transition.setInterpolator(new AnticipateOvershootInterpolator(1.0f));
+        transition.setDuration(1200);
+
+        TransitionManager.beginDelayedTransition(constraint,transition);
+        constSet.applyTo(constraint);
     }
 }
