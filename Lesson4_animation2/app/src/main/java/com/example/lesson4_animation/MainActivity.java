fa84498d6e0b2c7ffb395b466f031975c977fe3d
DariaTiessova <bermuds@inbox.ru>
2019-04-24 22:30:41 +0600

    container1/2

--- Lesson4_animation2/app/src/main/java/com/example/lesson4_animation/MainActivity.java
+++ Lesson4_animation2/app/src/main/java/com/example/lesson4_animation/MainActivity.java
@@ -10,22 +10,56 @@ import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.animation.AnticipateOvershootInterpolator;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+   /*private final ConstraintSet constSet = new ConstraintSet();
+    private final ConstraintSet constSet2 = new ConstraintSet();*/
+
+     ConstraintLayout constraint;
+     //ConstraintSet currentSet;
+    
+    boolean show=false;
+
+    ImageView imageV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      setContentView(R.layout.activity_main);
+     
+     constraint=(ConstraintLayout)findViewById(R.id.container);
+
+     // constSet.clone(constraint);
+     // constSet2.clone(MainActivity.this, R.layout.second_activity);
+
+     // currentSet=constSet;
 
 
+        imageV=findViewById(R.id.imageBack);
+        imageV.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+      
+            
+            if(show){
+                showComponents();
+            }else{
+                hideComponents();
+            }
+
+
+      /* currentSet=(currentSet== constSet ? constSet2 : constSet);
+
+            TransitionManager.beginDelayedTransition(constraint);
+            constSet.applyTo(constraint);*/
+        }
+    });
     }
 
-    public void onButtonClick(View view) {
-
-
+    private void hideComponents() {
+     show=false;
         ConstraintSet constSet = new ConstraintSet();
-        constSet.clone(this, R.layout.second_activity);
+        constSet.clone(this, R.layout.activity_main);
 
         ConstraintLayout constraint= (ConstraintLayout)findViewById(R.id.container);
         Transition transition = new ChangeBounds();
@@ -34,5 +68,23 @@ public class MainActivity extends AppCompatActivity {
 
         TransitionManager.beginDelayedTransition(constraint,transition);
         constSet.applyTo(constraint);
+
     }
+
+    private void showComponents() {
+
+     show=true;
+     ConstraintSet constSet = new ConstraintSet();
+        constSet.clone(this, R.layout.second_activity);
+
+         ConstraintLayout constraint= (ConstraintLayout)findViewById(R.id.container2);
+        Transition transition = new ChangeBounds();
+        transition.setInterpolator(new AnticipateOvershootInterpolator(1.0f));
+        transition.setDuration(1200);
+
+        TransitionManager.beginDelayedTransition(constraint,transition);
+        constSet.applyTo(constraint);
+    }
+
+
 }
